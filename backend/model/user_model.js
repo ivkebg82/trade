02e58a38369098950c14cb31c525be16b812649e
ivkebg82 <grcaivan@gmail.com>
2022-03-03 07:59:16 +0100

    add method in usermodel.js

--- backend/model/user_model.js
+++ backend/model/user_model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+
 const UserSchema = new mongoose.Schema({
         email: {
             type: String,
@@ -9,7 +10,7 @@ const UserSchema = new mongoose.Schema({
             lowercase: true,
             validate(value) {
                 if (!validator.isEmail(value)) {
-                    throw new Error('Email is ibvalid')
+                    throw new Error('Email is invalid')
                 }
             }
         },
@@ -27,6 +28,29 @@ const UserSchema = new mongoose.Schema({
     }
 
 );
+
+UserSchema.statics.findByCredentials = async (email, password) => {
+    const user = await UserModel.findOne({
+        email
+    });
+    if (!user) {
+        throw new Error('Invalid email!!!')
+    }
+    /*   const users = await UserModel.find();
+      const isValid = users.find((user) => user.password === password)
+      if (!isValid) {
+          throw new Error('Invalid password!')
+      }
+      console.log('user') */
+    const rightUser = await UserModel.findOne({
+        email: user.email,
+        password: password
+    })
+    if (!rightUser) {
+        throw new Error('Invalid password!')
+    }
+    console.log(rightUser)
+}
 
 const UserModel = mongoose.model('User', UserSchema);
 
